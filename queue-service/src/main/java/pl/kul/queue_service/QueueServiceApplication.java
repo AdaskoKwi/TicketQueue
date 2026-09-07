06467c39c3cfbdbd3c86a130a8f00ccf2fb2a6ce
adam <adamnowakhomm@gmail.com>
2026-09-07 08:39:27 +0200

    Redis setup

--- queue-service/src/main/java/pl/kul/queue_service/QueueServiceApplication.java
+++ queue-service/src/main/java/pl/kul/queue_service/QueueServiceApplication.java
@@ -1,7 +1,10 @@
 package pl.kul.queue_service;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 
 @SpringBootApplication
 public class QueueServiceApplication {
@@ -10,4 +13,18 @@ public class QueueServiceApplication {
 		SpringApplication.run(QueueServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner redisTestRunner(ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
+		return args -> {
+			String testKey = "test:key";
+			String testValue = "Hello Spring from Redis!";
+
+			reactiveRedisTemplate.opsForValue()
+					.set(testKey, testValue)
+					.doOnSuccess(success -> System.out.println("[REDIS SET] Key: " + testKey + " | Success: " + testValue))
+					.then(reactiveRedisTemplate.opsForValue().get(testKey))
+					.doOnNext(value -> System.out.println("[REDIS GET] Value: " + value))
+					.subscribe();
+		};
+	}
 }
