4408d83e6af815eb1720627664aab0369c3ebd39
adam <adamnowakhomm@gmail.com>
2026-09-19 11:53:05 +0200

    Fraud service project added

---
+++ fraud-insight-service/src/main/java/pl/kul/fraud_insight_service/FraudInsightServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.kul.fraud_insight_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudInsightServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudInsightServiceApplication.class, args);
+	}
+
+}
---
+++ fraud-insight-service/src/test/java/pl/kul/fraud_insight_service/FraudInsightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kul.fraud_insight_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudInsightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
